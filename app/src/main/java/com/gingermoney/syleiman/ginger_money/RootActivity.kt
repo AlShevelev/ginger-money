16423a939a36a95086aab8410e4d06b9c3778aee
Alexander Shevelev <al.e.shevelev@gmail.com>
2018-12-14 18:23:38 +0300

    App skeleton and resources were added

---
+++ app/src/main/java/com/gingermoney/syleiman/ginger_money/RootActivity.kt
@@ -0,0 +1,12 @@
+package com.gingermoney.syleiman.ginger_money
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
