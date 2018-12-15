87804b56f709c22c9c21d392f43ea7e6817e86ae
Alexander Shevelev <al.e.shevelev@gmail.com>
2018-12-15 17:51:39 +0300

    Brush app the project

--- app/src/main/java/com/gingermoney/syleiman/ginger_money/RootActivity.kt
+++ app/src/main/java/com/gingermoney/syleiman/ginger_money/RootActivity.kt
@@ -1,7 +1,7 @@
 package com.gingermoney.syleiman.ginger_money
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class RootActivity : AppCompatActivity() {
 
