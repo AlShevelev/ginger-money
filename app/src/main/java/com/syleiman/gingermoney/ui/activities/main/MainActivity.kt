80ef1ac08d8c46e0244c1fbd7fe67f147b98d1be
Alexander Shevelev <al.e.shevelev@gmail.com>
2019-02-07 19:01:57 +0300

    Toolbar for Main activity has been added

--- app/src/main/java/com/syleiman/gingermoney/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/syleiman/gingermoney/ui/activities/main/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainToolbar)
 
         App.injections.get<MainActivityComponent>().inject(this)
 
